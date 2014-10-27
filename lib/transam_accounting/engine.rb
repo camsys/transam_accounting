a0f7284b56d74669077eed2e64b65fc656edcbc1
Aaron Magil <amagil@camsys.com>
2014-10-27 13:22:11 -0400

    Improving engine.rb file to broadcast migrations, calculators to the app

--- lib/transam_accounting/engine.rb
+++ lib/transam_accounting/engine.rb
@@ -1,4 +1,22 @@
 module TransamAccounting
   class Engine < ::Rails::Engine
+    # Add a load path for this specific Engine
+    config.autoload_paths += %W(#{Rails.root}/app/calculators)
+
+    # Append migrations from the engine into the main app    
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s
+        config.paths["db/migrate"].expanded.each do |expanded_path|
+          app.config.paths["db/migrate"] << expanded_path
+        end
+      end
+    end
+
+    config.generators do |g|
+      g.test_framework      :rspec,        :fixture => false
+      g.fixture_replacement :factory_girl, :dir => 'spec/factories'
+      g.assets false
+      g.helper false
+    end
   end
 end
