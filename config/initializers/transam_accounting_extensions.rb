b88b94b35dd6cc3bdb785fe0f37635c2a4d1854f
Aaron Magil <amagil@camsys.com>
2014-10-28 15:32:04 -0400

    documentation improvement [ci skip]

--- config/initializers/transam_accounting_extensions.rb
+++ config/initializers/transam_accounting_extensions.rb
@@ -1,4 +1,15 @@
 # Technique caged from http://stackoverflow.com/questions/4460800/how-to-monkey-patch-code-that-gets-auto-loaded-in-rails
+# See http://guides.rubyonrails.org/configuring.html#initialization-events for
+# details on Initialization Events
+###############################################################################
+# 
+# Hook into the Rails initialization sequence.  to_prepare hook will fire once
+# on-boot in prod or test, but before each request in development
+# NOTE: All classes listed for class_eval here will be evaluated, including
+#   their modules, but the event fires before DB initialization, so any DB 
+#   calls WILL FAIL
+# 
+###############################################################################
 Rails.configuration.to_prepare do
   Asset.class_eval do
     include TransamAccounting::TransamDepreciable
