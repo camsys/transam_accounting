0f11792cd40af333e72712426ce630f2368bc90d
Lydia Chang <mathmerized@gmail.com>
2014-11-17 14:35:21 -0500

    gl account views and tabs

--- config/initializers/transam_accounting_extensions.rb
+++ config/initializers/transam_accounting_extensions.rb
@@ -2,17 +2,21 @@
 # See http://guides.rubyonrails.org/configuring.html#initialization-events for
 # details on Initialization Events
 ###############################################################################
-# 
+#
 # Hook into the Rails initialization sequence.  to_prepare hook will fire once
 # on-boot in prod or test, but before each request in development
 # NOTE: All classes listed for class_eval here will be evaluated, including
-#   their modules, but the event fires before DB initialization, so any DB 
+#   their modules, but the event fires before DB initialization, so any DB
 #   calls WILL FAIL
-# 
+#
 ###############################################################################
 Rails.configuration.to_prepare do
   Asset.class_eval do
     include TransamAccounting::TransamDepreciable
+    include TransamAccounting::TransamGlAccountableAsset
+  end
+  Grant.class_eval do
+    include TransamAccounting::TransamGlAccountableGrant
   end
   Organization.class_eval do
     include TransamAccounting::TransamAccountable
