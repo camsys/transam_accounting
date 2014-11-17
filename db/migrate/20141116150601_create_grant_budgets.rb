d3fd21f85d4483fc63da5f50d8750b70d11b22cd
Julian Ray <jray@camsys.com>
2014-11-17 14:58:38 -0500

    Add index and non-null constraints to join table

--- db/migrate/20141116150601_create_grant_budgets.rb
+++ db/migrate/20141116150601_create_grant_budgets.rb
@@ -4,10 +4,12 @@ class CreateGrantBudgets < ActiveRecord::Migration
     drop_join_table :general_ledger_accounts, :grants
 
     create_table :grant_budgets do |t|
-      t.references :general_ledger_account
-      t.references :grant
-      t.integer :amount
+      t.references :general_ledger_account, :null => :false
+      t.references :grant,                  :null => :false
+      t.integer :amount,                    :null => :false
     end
-
+   
+    add_index :grant_budgets,   [:general_ledger_account_id, :grant_id], :name => :grant_budgets_idx1
+   
   end
 end
