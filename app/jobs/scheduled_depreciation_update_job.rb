ca0075e547f885b731fbddd418ec3fa154f2fa62
Julian Ray <jray@camsys.com>
2014-12-24 08:54:45 -0500

    Add logging to ActivityLog table and send messages to managers informing them of the updates.

--- app/jobs/scheduled_depreciation_update_job.rb
+++ app/jobs/scheduled_depreciation_update_job.rb
@@ -20,6 +20,9 @@ class ScheduledDepreciationUpdateJob < Job
 
   def run
 
+    # Cache the system user
+    sys_user = get_system_user
+
     # Check each org and determine if they need to have their assets
     # depreciated
     Organization.all.each do |o|
@@ -39,14 +42,38 @@ class ScheduledDepreciationUpdateJob < Job
       # before the current depeciation date. We only need the object keys
       matches = org.assets.where('depreciable = true AND current_depreciation_date < ?', current_depreciation_date).pluck(:object_key)
 
-      Rails.logger.info "Updating #{matches.count} assets for #{org}"
+      if matches.empty?
+        msg = "#{matches.count} need to be depreciated."
+        Rails.logger.info msg
+        ActivityLog.create({:organization => org, :user => sys_user, :item_type => self.class.name, :activity => msg, :activity_time => Time.now})
+      else
+        Rails.logger.info "Updating #{matches.count} assets for #{org}"
 
-      matches.each do |key|
-        # Create a delayed job to perform the update
-        job = AssetValueUpdateJob.new(key)
-        Delayed::Job.enqueue job, :priority => JOB_PRIORITY
+        matches.each do |key|
+          # Create a delayed job to perform the update
+          job = AssetValueUpdateJob.new(key)
+          Delayed::Job.enqueue job, :priority => JOB_PRIORITY
+        end
+
+        # Insert a row in the activity log for this org
+        msg = "#{matches.count} assets were depreciated."
+        ActivityLog.create({:organization => org, :user => sys_user, :item_type => self.class.name, :activity => msg, :activity_time => Time.now})
+
+        # Send a message to the managers of the organization
+        message_subject = "Depreciated values of assets updated"
+        message_body = "#{matches.count} depreciable assets have had their book value updated. The new current depreciation date is #{current_depreciation_date}."
+        org.users.each do |user|
+          if user.has_role? :manager
+            message = Message.new
+            message.organization = org
+            message.to_user = user
+            message.from_user = sys_user
+            message.subject = message_subject
+            message.body = message_body
+            message.save
+          end
+        end
       end
-
 
     end
     Rails.logger.info "#{self.class.name} finished at #{Time.now}"
