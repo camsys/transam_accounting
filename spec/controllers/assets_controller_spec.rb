7e9066697c7f7b185e0f75860cefd23c0f20aa61
Lydia Chang <mathmerized@gmail.com>
2015-11-02 09:23:11 -0500

    cleanup rspec depreciation test

--- spec/controllers/assets_controller_spec.rb
+++ spec/controllers/assets_controller_spec.rb
@@ -39,14 +39,12 @@ RSpec.describe AssetsController, :type => :controller do
     # update required fields for a vehicle
     test_asset.update!(:depreciable => false, :manufacturer_id => 1, :manufacturer_model => 'VERSION-XX', :title_owner_organization_id => 1,:fta_ownership_type_id => 1, :fta_vehicle_type_id => 1, :fuel_type_id => 1, :serial_number => 'XXX-123456789', :vehicle_length => 40)
 
-    post :update_depreciation, use_route: :update_depreciation_inventory_path, :asset_depreciable_proxy => {:object_key => test_asset.object_key, :depreciable => '1', :depreciation_start_date => (Date.today - 1.day).strftime('%m/%d/%Y'), :salvage_value => '1111', :expected_useful_life => '111', :expected_useful_miles => '11111'}
+    post :update_depreciation, use_route: :update_depreciation_inventory_path, :asset_depreciable_proxy => {:object_key => test_asset.object_key, :depreciable => '1', :depreciation_start_date => (Date.today - 1.day).strftime('%m/%d/%Y'), :salvage_value => '1111'}
     test_asset.reload
 
     expect(test_asset.depreciable).to be true
     expect(test_asset.depreciation_start_date.to_date).to eq(Date.today - 1.day)
     expect(test_asset.salvage_value).to eq(1111)
-    expect(test_asset.expected_useful_life).to eq(111)
-    expect(test_asset.expected_useful_miles).to eq(11111)
     expect(test_asset.updator).to eq(subject.current_user)
   end
 end
