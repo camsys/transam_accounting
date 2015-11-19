e8d6eaa7bfbb4585978986c01445e9a8b4d8dfd6
Lydia Chang <mathmerized@gmail.com>
2015-11-19 13:25:01 -0500

    cleanup lookup

--- app/models/depreciation_interval_type.rb
+++ app/models/depreciation_interval_type.rb
@@ -3,4 +3,8 @@ class DepreciationIntervalType < ActiveRecord::Base
   # Active scope -- always use this scope in forms
   scope :active, -> { where(active: true) }
 
+  def to_s
+    name
+  end
+
 end
