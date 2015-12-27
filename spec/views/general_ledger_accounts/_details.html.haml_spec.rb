47bfe6d3e5c4c338a18db5e518ccec54acb5d3a0
Lydia Chang <mathmerized@gmail.com>
2015-12-26 20:34:08 -0500

    gla rspec views

---
+++ spec/views/general_ledger_accounts/_details.html.haml_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe "general_ledger_accounts/_details.html.haml", :type => :view do
+  it 'if no info' do
+    allow(controller).to receive(:current_ability).and_return(Ability.new(create(:admin)))
+    assign(:ledger_account, create(:general_ledger_account))
+    assign(:organization, create(:organization))
+    assign(:expenditure, Expenditure.new)
+    render
+
+    expect(rendered).to have_content('There are no assets associated with this general ledger account.')
+    expect(rendered).to have_content('There are no CapEx associated with this general ledger account.')
+  end
+end
