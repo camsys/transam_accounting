4425a10533a0df2e503584189e298d72d97be26f
Lydia Chang <mathmerized@gmail.com>
2016-02-24 10:32:39 -0500

    add codacy and simplecov

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,20 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
+
+require 'codacy-coverage'
+Codacy::Reporter.start
+
+require 'simplecov'
+SimpleCov.start 'rails' do
+  add_group "Calculators", "app/calculators"
+  add_group "File Handlers", "app/file_handlers"
+  add_group "Jobs", "app/jobs"
+  add_group "Reports", "app/reports"
+  add_group "Services", "app/services"
+  add_group "Searches", "app/searches"
+  add_group "Uploaders", "app/uploaders"
+end
+
 require 'spec_helper'
 require File.expand_path("../dummy/config/environment", __FILE__)
 require 'rspec/rails'
