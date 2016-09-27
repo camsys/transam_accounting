369d15c3ea6b3996af737bd63cb70d301bcf58be
Lydia Chang <mathmerized@gmail.com>
2016-09-27 13:54:54 -0400

    update test for removed columns

--- spec/views/funding_sources/_fund_details.html.haml_spec.rb
+++ spec/views/funding_sources/_fund_details.html.haml_spec.rb
@@ -2,11 +2,10 @@ require 'rails_helper'
 
 describe "funding_sources/_fund_details.html.haml", :type => :view do
   it 'info' do
-    test_fund = create(:funding_source, :state_match_required => 25, :local_match_required => 30)
+    test_fund = create(:funding_source, :match_required => 25)
     assign(:funding_source, test_fund)
     render
 
     expect(rendered).to have_content('25.000%')
-    expect(rendered).to have_content('30.000%')
   end
 end
