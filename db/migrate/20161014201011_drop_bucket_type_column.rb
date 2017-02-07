7ffe15afb3560846bef0d5a8030de9218fc81bf1
mmaranda-cs <mmaranda@camsys.com>
2017-02-07 12:07:47 -0500

    Removing some migration scripts from acounting because the models had moved to funding.

--- db/migrate/20161014201011_drop_bucket_type_column.rb
+++
@@ -1,6 +0,0 @@
-
-class DropBucketTypeColumn< ActiveRecord::Migration
-  def change
-    remove_column :funding_buckets, :bucket_type_id
-  end
-end
--- db/migrate/20161223153908_add_name_to_funding_buckets.rb
+++
@@ -1,9 +0,0 @@
-class AddNameToFundingBuckets < ActiveRecord::Migration
-
-  def change
-    unless column_exists? :funding_buckets, :name
-      add_column :funding_buckets, :name, :string, null: false, after: :fiscal_year
-    end
-  end
-
-end
