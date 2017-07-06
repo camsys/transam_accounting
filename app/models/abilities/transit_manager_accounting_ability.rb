5a6556c1c2e01d6504cf4fd860b799f183356bed
mathmerized <mathmerized@gmail.com>
2017-07-06 15:49:00 -0400

    destroy wip

--- app/models/abilities/transit_manager_accounting_ability.rb
+++ app/models/abilities/transit_manager_accounting_ability.rb
@@ -4,8 +4,15 @@ module Abilities
 
     def initialize(user)
       can :manage, Grant, :organization_id => user.organization_ids
+      cannot :destroy, Grant do |g|
+        !g.can_destroy?
+      end
 
       can :manage, GeneralLedgerAccount, :organization_id => user.organization_ids
+      cannot :destroy, GeneralLedgerAccount do |g|
+        !g.can_destroy?
+      end
+
 
     end
   end
