e00914c61886f07d11c4dc4ce8cb360f7da2f21e
mathmerized <mathmerized@gmail.com>
2017-11-08 14:09:37 -0500

    add data migrations gem and data migration for report

--- lib/transam_accounting/engine.rb
+++ lib/transam_accounting/engine.rb
@@ -13,6 +13,11 @@ module TransamAccounting
         config.paths["db/migrate"].expanded.each do |expanded_path|
           app.config.paths["db/migrate"] << expanded_path
         end
+        app.config.paths.add "db/data_migrations"
+        config.paths.add "db/data_migrations"
+        config.paths["db/data_migrations"].expanded.each do |expanded_path|
+          app.config.paths["db/data_migrations"] << expanded_path
+        end
       end
     end
 
