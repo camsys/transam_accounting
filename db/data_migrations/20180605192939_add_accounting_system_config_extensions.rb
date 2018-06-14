54be72289a21fa2047e8a898dbaa97ee041279ca
mathmerized <mathmerized@gmail.com>
2018-06-14 17:07:02 -0400

    remove transam accountable from system config extensions since orgs dont have chart of accounts currently TTPLAT-273

--- db/data_migrations/20180605192939_add_accounting_system_config_extensions.rb
+++ db/data_migrations/20180605192939_add_accounting_system_config_extensions.rb
@@ -3,7 +3,7 @@ class AddAccountingSystemConfigExtensions < ActiveRecord::DataMigration
     system_config_extensions = [
         {class_name: 'RehabilitationUpdateEvent', extension_name: 'TransamGlAccountableAssetEvent', active: true},
         {class_name: 'AssetsController', extension_name: 'TransamAccountingAssetsController', active: true},
-        {class_name: 'Organization', extension_name: 'TransamAccountable', active: true},
+        #{class_name: 'Organization', extension_name: 'TransamAccountable', active: true}, comment out temporarily as all orgs dont have COA
         {class_name: 'Policy', extension_name: 'TransamAccountingPolicy', active: true},
         {class_name: 'Vendor', extension_name: 'TransamAccountingVendor', active: true}
 
