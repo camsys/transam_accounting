09d10ac2d4efd6b5e632719392edd98231925eea
mathmerized <mathmerized@gmail.com>
2018-06-20 12:17:30 -0400

    forgot to require new accounting mixin

--- lib/transam_accounting.rb
+++ lib/transam_accounting.rb
@@ -1,4 +1,5 @@
 require "transam_accounting/engine"
+require "transam_accounting/transam_valuable"
 require "transam_accounting/transam_depreciable"
 require "transam_accounting/transam_accountable"
 require "transam_accounting/transam_accounting_policy"
