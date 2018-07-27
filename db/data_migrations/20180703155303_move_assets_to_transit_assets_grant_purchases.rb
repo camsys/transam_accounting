8eae23450d799635080a9b5a3e558af6ca2aec2e
mathmerized <mathmerized@gmail.com>
2018-07-27 10:28:42 -0400

    cleanup data loading TTPLAT-234

--- db/data_migrations/20180703155303_move_assets_to_transit_assets_grant_purchases.rb
+++ db/data_migrations/20180703155303_move_assets_to_transit_assets_grant_purchases.rb
@@ -1,7 +1,7 @@
 class MoveAssetsToTransitAssetsGrantPurchases < ActiveRecord::DataMigration
   def up
     GrantPurchase.all.each do |gp|
-      gp.update_columns(transam_asset_id: TransitAsset.find_by(asset_id: gp.asset_id).transam_asset.id)
+      gp.update_columns(transam_asset_id: TransitAsset.find_by(asset_id: gp.asset_id).try(:transam_asset).try(:id))
     end
   end
 end
