58d8921cbc8fc99244d79a3c04d17a7a4406f8d6
mathmerized <mathmerized@gmail.com>
2018-08-02 14:26:44 -0400

    cleanup rails admin and move toward engines

--- config/initializers/transam.rb
+++ config/initializers/transam.rb
@@ -1,2 +1,3 @@
 Rails.application.config.asset_purchase_source = 'FundingSource'
 Rails.application.config.grant_source = 'FundingSource'
+Rails.application.config.rails_admin_accounting_lookup_tables = ['DepreciationIntervalType', 'ExpenseType']
