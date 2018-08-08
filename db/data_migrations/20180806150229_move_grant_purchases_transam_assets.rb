773a882bf0430fbefd7f3680270e61f33b4ee728
mathmerized <mathmerized@gmail.com>
2018-08-08 12:07:47 -0400

    move data scripts to end of other data migrations

--- db/data_migrations/20180806150229_move_grant_purchases_transam_assets.rb
+++ db/data_migrations/20180806150229_move_grant_purchases_transam_assets.rb
@@ -1,4 +1,4 @@
-class MoveAssetsToTransitAssetsGrantPurchases < ActiveRecord::DataMigration
+class MoveGrantPurchasesTransamAssets < ActiveRecord::DataMigration
   def up
     GrantPurchase.all.each do |gp|
       gp.update_columns(transam_asset_id: TransitAsset.find_by(asset_id: gp.asset_id).try(:transam_asset).try(:id))
