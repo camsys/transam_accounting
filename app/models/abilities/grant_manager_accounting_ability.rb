49733e907023ba36542ad8cd6c21b6912634317e
mathmerized <mathmerized@gmail.com>
2019-01-23 11:22:37 -0500

    TTPLAT-550 add org permissions to grants

--- app/models/abilities/grant_manager_accounting_ability.rb
+++ app/models/abilities/grant_manager_accounting_ability.rb
@@ -2,10 +2,17 @@ module Abilities
   class GrantManagerAccountingAbility
     include CanCan::Ability
 
-    def initialize(user)
+    def initialize(user, organization_ids=[])
 
-      can :manage, Grant
+      if organization_ids.empty?
+        organization_ids = user.organization_ids
+      end
+
+      can :manage, Grant do |grant|
+        user.viewable_organization_ids.include? grant.owner_id
+      end
 
     end
+
   end
 end
