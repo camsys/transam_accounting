b43d3a8a3946ae05236c89ea53fac502e60903ce
mathmerized <mathmerized@gmail.com>
2019-01-29 10:43:44 -0500

    dont do depreciation job on assets in transfer TTPLAT-691

--- app/jobs/asset_depreciation_expense_update_job.rb
+++ app/jobs/asset_depreciation_expense_update_job.rb
@@ -10,7 +10,7 @@ class AssetDepreciationExpenseUpdateJob < ActivityJob
   def run
     asset_klass = Rails.application.config.asset_base_class_name.constantize
 
-    asset_klass.where.not(current_depreciation_date: Policy.first.current_depreciation_date).each do |a|
+    asset_klass.not_in_transfer.where.not(current_depreciation_date: Policy.first.current_depreciation_date).each do |a|
       asset = asset_klass.get_typed_asset(a)
       asset.update_asset_book_value
       asset.save(validate: false)
