f1453f29c09449720a33f8820db884527df7476a
Alex Andrade <aandrade@camsys.com>
2019-03-05 14:05:18 -0500

    Fix transam_accounting config to get prepare_rspec to finish. Tests will need to be refactored.

--- spec/dummy/config/initializers/transam_core_extensions.rb
+++ spec/dummy/config/initializers/transam_core_extensions.rb
@@ -4,3 +4,7 @@ Rails.application.config.transam_transit_rail = false
 # Defines services to use
 Rails.application.config.new_user_service = "NewUserService"
 Rails.application.config.policy_analyzer = "TransitPolicyAnalyzer"
+
+# Base class name is to determine base asset class. Seed class is used to determine seed that gets typed (or very specific) asset class.
+Rails.application.config.asset_base_class_name = 'TransamAsset'
+Rails.application.config.asset_seed_class_name = 'FtaAssetClass'
