430c96e7634200579c83ce43526957539f54d3b0
mathmerized <mathmerized@gmail.com>
2019-04-02 08:20:22 -0400

    TTPLAT-246 finish up TDC funding source type data seed migrations

--- db/data_migrations/20190325185531_add_tdc_funding_source_type.rb
+++ db/data_migrations/20190325185531_add_tdc_funding_source_type.rb
@@ -1,6 +1,6 @@
 class AddTdcFundingSourceType < ActiveRecord::DataMigration
   def up
     tdc_type = FundingSourceType.create!(name: 'TDC', description: 'Transportation Development Credits', active: false)
-    FundingSource.create!(name: 'TDC', description: 'Transportation Development Credits', funding_source_type: tdc_type, match_required: 100)
+    FundingSource.create!(name: 'TDC', description: 'Transportation Development Credits', funding_source_type: tdc_type, match_required: 20, discretionary_fund: true, formula_fund: false)
   end
 end
