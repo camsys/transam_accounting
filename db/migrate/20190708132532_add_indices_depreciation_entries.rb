4673728ca311d0020f11f9591359e2259a5dd8b8
mathmerized <mathmerized@gmail.com>
2019-07-08 09:42:48 -0400

    fix index name TTPLAT-1055

--- db/migrate/20190708132532_add_indices_depreciation_entries.rb
+++ db/migrate/20190708132532_add_indices_depreciation_entries.rb
@@ -1,5 +1,5 @@
 class AddIndicesDepreciationEntries < ActiveRecord::Migration[5.2]
   def change
-    add_index(:depreciation_entries, [:transam_asset_id, :event_date, :description], unique: true)
+    add_index(:depreciation_entries, [:transam_asset_id, :event_date, :description], unique: true, name: 'transam_asset_event_date_description_uniq_idx')
   end
 end
