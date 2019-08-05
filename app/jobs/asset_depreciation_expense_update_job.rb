a44414fa6ae9d613cabb21660304cded6a4a1984
mathmerized <mathmerized@gmail.com>
2019-08-05 11:06:28 -0400

    TTPLAT-1055 fix bad negative

--- app/jobs/asset_depreciation_expense_update_job.rb
+++ app/jobs/asset_depreciation_expense_update_job.rb
@@ -10,7 +10,7 @@ class AssetDepreciationExpenseUpdateJob < ActivityJob
   def run
     asset_klass = Rails.application.config.asset_base_class_name.constantize
 
-    asset_klass.not_in_transfer.where.not(current_depreciation_date: Policy.first.current_depreciation_date).or(asset_klass.not_in_transfer.where.not(current_depreciation_date: nil)).each do |a|
+    asset_klass.not_in_transfer.where.not(current_depreciation_date: Policy.first.current_depreciation_date).or(asset_klass.not_in_transfer.where(current_depreciation_date: nil)).each do |a|
       asset = asset_klass.get_typed_asset(a)
       asset.update_asset_book_value
     end
