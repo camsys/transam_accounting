d3b6027bb346b3c572511ab4a69588bc68146951
NJBL <beriolebeau.n@husky.neu.edu>
2020-09-29 15:47:25 -0400

    remove unnecessary check in programs index actions test

--- spec/views/funding_sources/_index_actions.html.haml_spec.rb
+++ spec/views/funding_sources/_index_actions.html.haml_spec.rb
@@ -5,6 +5,7 @@ describe "funding_sources/_index_actions.html.haml", :type => :view do
     allow(controller).to receive(:current_ability).and_return(Ability.new(create(:admin)))
     render
 
-    expect(rendered).to have_field('funding_source_type_id')
+    # these filters have been removed as a part of the table UI refresh, and this check is no longer needed
+    # expect(rendered).to have_field('funding_source_type_id')
   end
 end
